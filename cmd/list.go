a33fdfe4d504455dfa4bae0e1cbca192c3bf17e7
Lewis O'Driscoll <lewis.odriscoll@dmwgroup.co.uk>
2021-05-02 14:41:13 +0100

    Add list command

---
+++ cmd/list.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all lambdas",
+	Run: func(cmd *cobra.Command, args []string) {
+		lambdas, err := finder.FindLambdas([]string{})
+		if err != nil {
+			printer.PrintErr(err)
+			os.Exit(1)
+		}
+
+		if len(lambdas) == 0 {
+			printer.Println("🕵️ No lambdas found")
+			return
+		}
+
+		printer.Println("🕵️ Found lambdas:")
+		for _, lambda := range lambdas {
+			printer.Println(lambda)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
